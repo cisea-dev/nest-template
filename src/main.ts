5cccae048d21c479f04f16da9882e1d3e5492be4
ini-agung <cisea.dev@gmail.com>
2023-06-20 14:48:58 +0700

    Update Dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {RolesGuard} from "./auth/roles.guard";
+import { RolesGuard } from "./auth/roles.guard";
 import {
   FastifyAdapter,
   NestFastifyApplication,
@@ -8,15 +8,17 @@ import {
 import helmet from '@fastify/helmet';
 import { join } from 'path';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import {ValidationPipe} from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
+import fastifyCsrf from '@fastify/csrf-protection';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
-      AppModule,
-      new FastifyAdapter({logger: false})
+    AppModule,
+    new FastifyAdapter({ logger: false }),
   );
+  app.enableCors();
   app.useStaticAssets({
     root: join(__dirname, '..', 'public'),
     prefix: '/public/',
@@ -27,6 +29,8 @@ async function bootstrap() {
     },
     templates: join(__dirname, '..', 'views'),
   });
+
+  await app.register(fastifyCsrf);
   await app.register(helmet);
   await app.listen(3000);
   app.useGlobalGuards(new RolesGuard());
